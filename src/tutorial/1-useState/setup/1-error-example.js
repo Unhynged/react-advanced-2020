fa673618d6c29d8da03a48bfd48c14b6253bb4d9
Unhynged <unhynged@protonmail.com>
2022-06-23 14:42:15 +1000

    Modified 1-error-example.js and 2-useState-basics.js; useState Basics -> General Rules of Hooks

--- src/tutorial/1-useState/setup/1-error-example.js
+++ src/tutorial/1-useState/setup/1-error-example.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React from "react";
 
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
+  let title = "random title";
+  const handleClick = () => {
+    title = 'hello people'
+    console.log(title);
+  };
+  // We don't see the changes as we're not currently re-rendering the component. We would want to keep the value between the renders but also we would want to trigger that rerender and this is where the useState hook comes into play. 
+
+  return (
+    <React.Fragment>
+      <h2>{title}</h2>
+      <button type="button" className="btn" onClick={handleClick}>
+        change title
+      </button>
+    </React.Fragment>
+  );
 };
 
 export default ErrorExample;
